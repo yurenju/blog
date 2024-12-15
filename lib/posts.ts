d3704c48ff2afaf6e15c679ef2a83008099d2fac
yurenju <yurenju@gmail.com>
2024-12-15 21:56:45 +0800

    fix: moved posts to public folder

--- lib/posts.ts
+++ lib/posts.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 
-const postsDirectory = path.join(process.cwd(), "content/posts");
+const postsDirectory = path.join(process.cwd(), "public/posts");
 
 interface PostMetadata {
   slug: string;
